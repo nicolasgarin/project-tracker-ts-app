5c51fb2db486b9930e8bd07232a28c7911eb55a3
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-05-13 10:43:01 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './main.scss'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from './pages/Home.tsx'
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+const router = createBrowserRouter([
+  {
+    path: "/project-tracker-app/",
+    element: <App />,
+    children: [
+      {
+        path: "/project-tracker-app/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
