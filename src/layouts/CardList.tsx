3497912464085f67cb4a5d7305157970bb5c431e
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-05-15 09:28:31 -0300

    Update CardList.tsx

--- src/layouts/CardList.tsx
+++ src/layouts/CardList.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-export default function CardList({children}) {
+export const CardList = (props) => {
+  const { children } = props;
+
   return (
-    <div className="cardlist card-container d-flex">
-        <div className="row">
-            {children}
-        </div>
-    </div>
+    <>
+      <div className="cardlist card-container d-flex">
+          <div className="row">
+              {children}
+          </div>
+      </div>
+    </>
   )
 }
